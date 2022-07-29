12c4d98bde22f344d94cb4a989a8fec872fdb8f1
aperezlindo <abel.pzl.com@gmail.com>
2022-07-29 13:53:59 -0300

    Simplificacion del formulario

--- src/Form/SttConfigurationForm.php
+++ src/Form/SttConfigurationForm.php
@@ -27,9 +27,6 @@ class SttConfigurationForm extends ConfigFormBase {
       /** @var \Drupal\Core\Utility\Token $token */
       $tokens = \Drupal::token();
       $config_manager   = \Drupal::service('simple_twitter_tweet.config_manager');
-      $types            = $config_manager::getNodeTypesIds();
-      $saved_content_id = $config_manager::get('content');
-      $image_styles     = $config_manager::getImageStylesOptions();
 
       $form['sections'] = [
         '#type'         => 'vertical_tabs',
@@ -37,112 +34,44 @@ class SttConfigurationForm extends ConfigFormBase {
         '#default_tab'  =>'edit-content-box'
       ];
 
-      if(empty($saved_content_id)){
-        $form['content_box'] = [
-          '#type'         => 'details',
-          '#title'        => t('Content Settings'),
-          '#group'        => 'sections',
-          '#description'  => t('
-            Select the type of content and the 
-            fields that will be used for sharing on 
-            social networks when the content is created.'
-          ),
-        ];
-        $form['content_box']['content'] = [
-          '#type'         => 'select',
-          '#title'        => t('Content type'),
-          '#options'      => $types,
-          '#empty_option' => t('Select'),
-          '#description'  => t('
-            Select the type of content you want to use to be published 
-            on social networks at the time of its creation.
-          '),
-        ];
-        $form['content_box']['save_content_id'] = [
-          '#type'   => 'submit',
-          '#name'   => 'save_content_id',
-          '#value'  => t('save content config')
-        ]; 
-      }
-      else {
-        $options = $config_manager::getFieldsOptions($saved_content_id);
-        $form['content_box'] = [
-          '#type'         => 'details',
-          '#title'        => t('Content Settings'),
-          '#group'        => 'sections',
-          '#description'  => t( 
-            'The content type used is :@content_type, you can select the fields from which the social post will be created.',
-            [':@content_type' => $types[$saved_content_id]]
-          ),
-          
-        ];
+      $form['content_box'] = [
+        '#type'         => 'details',
+        '#title'        => t('Content Settings'),
+        '#group'        => 'sections',
+        '#description'  => t('
+          Select the type of content and the 
+          fields that will be used for sharing on 
+          social networks when the content is created.'
+        ),
+      ];
 
-        $form['content_box']['body'] = [
-          '#type'           => 'select',
-          '#title'          => t('Field to use to generate the body of the post'),
-          '#options'        => $options['body'],
-          '#default_value'  => $config_manager::get('body') ?? '',
-          '#empty_option'   => t('Select'),
-          '#description'    => t(
-            'Select the field you want to use
-            as a font for the body of the tweet. If the text is too long, it will be cut off.'
-          ),
-        ];
-        $form['content_box']['body_use_summary'] = [
-          '#type'           => 'checkbox',
-          '#title'          => t('Use summary if available for selected field in tweet text.'),
-          '#default_value'  => $config_manager::get('body_use_summary') ?? '',
-          '#states'         => [
-            'invisible' => [':input[name="body"]' => ['value' => '']],
-          ],
-        ];
+      $form['content_box']['content'] = [
+        '#type'           => 'textfield',
+        '#title'          => t('Content bundle'),
+        '#description'    => t('Enter the content bundle'),
+        '#default_value'  => $config_manager::get('content')
+      ];
+      $form['content_box']['body'] = [
+        '#type'           => 'textfield',
+        '#title'          => t('Text field'),
+        '#description'    => t('Enter the text field to use. It is only allowed to use the title of the content or a field of typestring, text, text_long or text_with_summary'),
+        '#default_value'  => $config_manager::get('body') ?? '',
+      ];
 
-        $form['content_box']['body_concat_url'] = [
-          '#type'   => 'checkbox',
-          '#title'  => t('Concat content url to tweet text.'),
-          '#default_value' => $config_manager::get('body_concat_url') ?? '',
-        ];
+      $form['content_box']['body_use_summary'] = [
+        '#type'           => 'checkbox',
+        '#title'          => t('Use summary if available for selected field in tweet text.'),
+        '#default_value'  => $config_manager::get('body_use_summary') ?? '',
+        '#states'         => [
+          'invisible' => [':input[name="body"]' => ['value' => '']],
+        ],
+      ];
 
-        $form['content_box']['image'] = [
-          '#type'           => 'select',
-          '#title'          => t('Field to use to generate the image of the post'),
-          '#options'        => $options['image'],
-          '#description'    => t('Select the field you want to use for the post image.'),
-          '#default_value'  => $config_manager::get('image') ?? '',
-          '#empty_option'   => t('Select')
-        ];
-        $form['content_box']['image_style'] = [
-          '#type'           => 'select',
-          '#title'          => t('Post Image Style'),
-          '#options'        => $image_styles,
-          '#default_value'  => $config_manager::get('image_style') ?? '',
-          '#empty_option'   => t('Select'),
-          '#description'    => t(
-            'Enter the image style you want to use, 
-            if you do not select any option the original image will be used.'
-          ),
-          '#states'         => [
-            'invisible' => [':input[name="image"]' => ['value' => '']],
-          ],
-        ];
-        $form['content_box']['options']['delete'] = [
-          '#type'   => 'submit',
-          '#name'   => 'delete_content_config',
-          '#value'  => t('Delete this setting')
-        ]; 
-        $form['content_box']['options']['perview'] = [
-          '#type'   => 'submit',
-          '#name'   => 'content_config_preview',
-          '#value'  => t('Save these settings and preview')
-        ];
-        $preview = $config_manager::get('preview_markup') ?? '';
-        if(!empty($preview)){
-          $form['content_box']['preview_markup'] = [
-            '#markup' => $preview,
-          ];
-          $config_manager::set('preview_markup', ''); 
-        }      
-      }
+      $form['content_box']['body_concat_url'] = [
+        '#type'   => 'checkbox',
+        '#title'  => t('Concat content url to tweet text.'),
+        '#default_value' => $config_manager::get('body_concat_url') ?? '',
+      ];
 
       $form['twitter'] = [
         '#type'   => 'details',
